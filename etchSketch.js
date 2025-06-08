b76e67f9d3b43918e2a7cb47502d8da30ea09f0a
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-08 19:59:31 +0100

    Add functionality for setting grid size.

--- etchSketch.js
+++ etchSketch.js
@@ -1,17 +1,43 @@
 const grid = document.querySelector("#grid");
+const numberOfSquaresButton = document.querySelector("#number-of-squares");
 let gridSize = 16;
-const squareSize = '20px';
+const GRID_WIDTH = 320;
 
-for (var i = 1; i <= gridSize * gridSize; i++) {
-    let square = document.createElement("div");
-    square.className = "grid-div";
-    square.style.width = squareSize;
-    square.style.height = squareSize;
-    square.style.flexShrink = 0;
-    grid.appendChild(square);
-    square.addEventListener('mouseover', () => square.style.backgroundColor = getRandomColour());
+numberOfSquaresButton.addEventListener('click', () => {
+    let numberOfSquares = parseInt(prompt("How many squares per side for the grid?", 16), 10);
+    if (numberOfSquares > 100)
+        numberOfSquares = 100;
+    else if (numberOfSquares < 1)
+        numberOfSquares = 1;
+    gridSize = numberOfSquares;
+    setGrid();
+});
+
+function setSquareSize() {
+    return GRID_WIDTH / gridSize;
+}
+
+function setGrid() {
+    clearGrid();
+    let squareSize = setSquareSize(gridSize);
+    for (var i = 1; i <= gridSize * gridSize; i++) {
+        let square = document.createElement("div");
+        square.className = "grid-div";
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        square.style.flexShrink = 0;
+        grid.appendChild(square);
+        square.addEventListener('mouseover', () => square.style.backgroundColor = getRandomColour());
+    }
+}
+
+function clearGrid() {
+    let squares = document.querySelectorAll(".grid-div");
+    squares.forEach(square => square.remove());
 }
 
 function getRandomColour() {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+setGrid();
