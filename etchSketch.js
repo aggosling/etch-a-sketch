c6bd4a65f7284e5bee499b1e4dfba0216e4dcfc9
Andy Gosling <45098094+aggosling@users.noreply.github.com>
2025-06-08 20:41:47 +0100

    Add opacity change.

--- etchSketch.js
+++ etchSketch.js
@@ -9,25 +9,46 @@ numberOfSquaresButton.addEventListener('click', () => {
         numberOfSquares = 100;
     else if (numberOfSquares < 1)
         numberOfSquares = 1;
-    gridSize = numberOfSquares;
-    setGrid();
+    if (numberOfSquares) {
+        gridSize = numberOfSquares;
+        setGrid();
+    }
 });
 
 function setSquareSize() {
     return GRID_WIDTH / gridSize;
 }
 
+function getOpacity(id) {
+    const square = document.querySelector(`#${id}`);
+    return square.style.opacity;
+}
+
+function increaseOpacity(opacity) {
+    return opacity += 0.1;
+}
+
+function createSquare(index, squareSize) {
+    let square = document.createElement("div");
+    square.setAttribute("id", "square-" + index);
+    square.className = "grid-div";
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
+    square.style.flexShrink = 0;
+    square.style.opacity = 0;
+    grid.appendChild(square);
+    return square;
+}
+
 function setGrid() {
     clearGrid();
-    let squareSize = setSquareSize(gridSize);
-    for (var i = 1; i <= gridSize * gridSize; i++) {
-        let square = document.createElement("div");
-        square.className = "grid-div";
-        square.style.width = squareSize + "px";
-        square.style.height = squareSize + "px";
-        square.style.flexShrink = 0;
-        grid.appendChild(square);
-        square.addEventListener('mouseover', () => square.style.backgroundColor = getRandomColour());
+    const squareSize = setSquareSize(gridSize);
+    for (let i = 1; i <= gridSize * gridSize; i++) {
+        let square = createSquare(i, squareSize);
+        square.addEventListener('mouseover', () => {
+            square.style.opacity = increaseOpacity(Number(getOpacity(square.getAttribute("id"))));
+            square.style.backgroundColor = getRandomColour();
+        });
     }
 }
 
